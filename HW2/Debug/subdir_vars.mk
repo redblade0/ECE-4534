37a961b62d47259e4db160f77f8afeaf8a65999e
redblade0 <benseidel03@gmail.com>
2026-09-24 21:13:00 -0400

    renamed file

---
+++ HW2/Debug/subdir_vars.mk
@@ -0,0 +1,67 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../mspm0g3507.cmd 
+
+SYSCFG_SRCS += \
+../blink_led.syscfg 
+
+C_SRCS += \
+./ti_msp_dl_config.c \
+../main-blinky.c \
+../main.c \
+../timestamp.c 
+
+GEN_FILES += \
+./ti_msp_dl_config.c 
+
+C_DEPS += \
+./ti_msp_dl_config.d \
+./main-blinky.d \
+./main.d \
+./timestamp.d 
+
+OBJS += \
+./ti_msp_dl_config.o \
+./main-blinky.o \
+./main.o \
+./timestamp.o 
+
+GEN_MISC_FILES += \
+./ti_msp_dl_config.h \
+./Event.dot 
+
+OBJS__QUOTED += \
+"ti_msp_dl_config.o" \
+"main-blinky.o" \
+"main.o" \
+"timestamp.o" 
+
+GEN_MISC_FILES__QUOTED += \
+"ti_msp_dl_config.h" \
+"Event.dot" 
+
+C_DEPS__QUOTED += \
+"ti_msp_dl_config.d" \
+"main-blinky.d" \
+"main.d" \
+"timestamp.d" 
+
+GEN_FILES__QUOTED += \
+"ti_msp_dl_config.c" 
+
+SYSCFG_SRCS__QUOTED += \
+"../blink_led.syscfg" 
+
+C_SRCS__QUOTED += \
+"./ti_msp_dl_config.c" \
+"../main-blinky.c" \
+"../main.c" \
+"../timestamp.c" 
+
+
